504baa7673c3d8ff397e72fabb4a9104007aa0f1
Vishal Maurya <vishalmr96@gmail.com>
2019-09-01 18:32:26 +0530

    Added the files

---
+++ i3/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+# pkill conky 
+
+# conky -c ~/.conky/horical.conkyrc 
+# conky -c ~/.conky/syclo-crimson-bottomleft.conkyrc
+# conky -c  ~/.conky/shailen.conf 
+# compstrt 
+
+# rofi -show window -kb-accept-entry "Ctrl-Alt-q,Return" -kb-row-down "Ctrl-Alt-q" -kb-cancel "Ctrl-Alt-Escape,Escape"
+
